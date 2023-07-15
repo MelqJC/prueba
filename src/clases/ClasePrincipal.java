fdf38ea7233a63412e0d109e2817bb1ff79fbb7c
MelqJC <melquiadesjc1@gmail.com>
2023-07-15 15:20:40 -0600

    modificacion clase principal

--- src/clases/ClasePrincipal.java
+++ src/clases/ClasePrincipal.java
@@ -3,6 +3,7 @@ package clases;
 public class ClasePrincipal {
 
     public static void main(String[] args) {
+        //Clase principal, se llaman las clases creada
         ClasePadre_Abstracta mensajero = new ClaseHija_Consulta();
         mensajero.setSaldo(500);
         mensajero.Operaciones();
